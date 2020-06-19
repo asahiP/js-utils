a592baf979fbf5b15846d6e34541a51756b12432
Luke Pan <asahi0749@gmail.com>
2020-06-19 11:29:34 +0800

    修正版本号、修正numToUppercase函数不正确描述

--- lib/numToUppercase.ts
+++ lib/numToUppercase.ts
@@ -3,7 +3,7 @@
 import { chunk } from './chunk'
 
 /**
- * @description 转换范围 `10^14[小数点]10^-2`
+ * @description 转换范围 `10^44[小数点]10^-2`
  * @param {string} number 需要转换的数字字符串
  * @param {boolean} isSimplifiedChinese 是否为简体
  * @returns {Array} 包含转换后整数位和小数位字符串的数组
